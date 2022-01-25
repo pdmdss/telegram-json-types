40de61d6f5524bd5fa7ca93c5c22613c86697f10
soshi1822 <soshi1822@gmail.com>
2022-01-25 18:05:10 +0900

    add: earthquake-counts schema 追加

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,6 @@
 import * as EarthquakeInformation from './schema/earthquake-information';
 import * as EarthquakeExplanation from './schema/earthquake-explanation';
+import * as EarthquakeCounts from './schema/earthquake-counts';
 import * as EewInformation from './schema/eew-information';
 import * as WeatherTyphoon from './schema/eew-information';
 import * as TsunamiInformation from './schema/tsunami-information';
@@ -7,6 +8,7 @@ import * as TsunamiInformation from './schema/tsunami-information';
 export {
   EarthquakeInformation,
   EarthquakeExplanation,
+  EarthquakeCounts,
   EewInformation,
   TsunamiInformation,
   WeatherTyphoon
---
+++ types/schema/earthquake-counts/1.0.0.d.ts
@@ -0,0 +1,69 @@
+import { TelegramJSONMain } from '../../main';
+
+export namespace EarthquakeCounts {
+  export interface Schema {
+    type: 'earthquake-counts';
+    version: '1.0.0';
+  }
+
+  export interface EarthquakeCountTargetTime {
+    start: string;
+    end: string;
+  }
+
+  export interface EarthquakeCountValues {
+    all: string | null;
+    felt: string | null;
+  }
+
+  export interface EarthquakeCount {
+    type: '１時間地震回数' | '累計地震回数' | '地震回数';
+    targetTime: EarthquakeCountTargetTime;
+    values: EarthquakeCountValues;
+  }
+
+  export interface Comments {
+    free: string;
+  }
+
+  export interface PublicBody {
+    earthquakeCounts?: EarthquakeCount;
+    nextAdvisory?: string;
+    text?: string;
+    comments?: Comments;
+  }
+
+  export interface CancelBody {
+    text: string;
+  }
+
+  export interface Public extends TelegramJSONMain {
+    _schema: Schema;
+    type: '地震回数に関する情報';
+    title: '地震回数に関する情報';
+    infoType: '発表' | '訂正';
+    targetDateTimeDubious: never;
+    targetDuration: never;
+    validDateTime: never;
+    eventId: string;
+    serialNo: string;
+    infoKind: '地震回数情報';
+    body: PublicBody;
+  }
+
+  export interface Cancel extends TelegramJSONMain {
+    _schema: Schema;
+    type: '地震回数に関する情報';
+    title: '地震回数に関する情報';
+    infoType: '取消';
+    targetDateTimeDubious: never;
+    targetDuration: never;
+    validDateTime: never;
+    eventId: string;
+    serialNo: string;
+    infoKind: '地震回数情報';
+    body: CancelBody;
+  }
+
+  export type Main = Public | Cancel;
+}
