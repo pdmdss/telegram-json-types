f5837ce4b075528304420284d859fdd190001dd9
soshi1822 <soshi1822@gmail.com>
2022-01-25 18:09:27 +0900

    add: earthquake-hypocenter-update schema 追加

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,7 @@
 import * as EarthquakeInformation from './schema/earthquake-information';
 import * as EarthquakeExplanation from './schema/earthquake-explanation';
 import * as EarthquakeCounts from './schema/earthquake-counts';
+import * as EarthquakeHypocenterUpdate from './schema/earthquake-hypocenter-update';
 import * as EewInformation from './schema/eew-information';
 import * as WeatherTyphoon from './schema/eew-information';
 import * as TsunamiInformation from './schema/tsunami-information';
@@ -9,6 +10,7 @@ export {
   EarthquakeInformation,
   EarthquakeExplanation,
   EarthquakeCounts,
+  EarthquakeHypocenterUpdate,
   EewInformation,
   TsunamiInformation,
   WeatherTyphoon
---
+++ types/schema/earthquake-hypocenter-update/1.0.0.d.ts
@@ -0,0 +1,53 @@
+import { TelegramJSONMain } from '../../main';
+import { Earthquake } from '../component/earthquake';
+
+export namespace EarthquakeHypocenterUpdate {
+  export interface Schema {
+    type: 'earthquake-hypocenter-update';
+    version: '1.0.0';
+  }
+
+  export interface Comments {
+    free: string;
+  }
+
+  export interface PublicBody {
+    earthquake: Earthquake;
+    text: string;
+    comments?: Comments;
+  }
+
+  export interface CancelBody {
+    text: string;
+  }
+
+  export interface Public extends TelegramJSONMain {
+    _schema: Schema;
+    type: '顕著な地震の震源要素更新のお知らせ';
+    title: '顕著な地震の震源要素更新のお知らせ';
+    infoType: '発表' | '訂正';
+    targetDateTimeDubious: never;
+    targetDuration: never;
+    validDateTime: never;
+    eventId: string;
+    serialNo: null;
+    infoKind: '震源要素更新のお知らせ';
+    body: PublicBody;
+  }
+
+  export interface Cancel extends TelegramJSONMain {
+    _schema: Schema;
+    type: '顕著な地震の震源要素更新のお知らせ';
+    title: '顕著な地震の震源要素更新のお知らせ';
+    infoType: '取消';
+    targetDateTimeDubious: never;
+    targetDuration: never;
+    validDateTime: never;
+    eventId: string;
+    serialNo: null;
+    infoKind: '震源要素更新のお知らせ';
+    body: CancelBody;
+  }
+
+  export type Main = Public | Cancel;
+}
