87f6fd9b07f9e44a5a91528dff9abdcc3e465abe
soshi1822 <soshi1822@gmail.com>
2022-01-25 18:16:51 +0900

    add: earthquake-nankai schema 追加

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,6 +2,7 @@ import * as EarthquakeInformation from './schema/earthquake-information';
 import * as EarthquakeExplanation from './schema/earthquake-explanation';
 import * as EarthquakeCounts from './schema/earthquake-counts';
 import * as EarthquakeHypocenterUpdate from './schema/earthquake-hypocenter-update';
+import * as EarthquakeNankai from './schema/earthquake-nankai';
 import * as EewInformation from './schema/eew-information';
 import * as WeatherTyphoon from './schema/eew-information';
 import * as TsunamiInformation from './schema/tsunami-information';
@@ -11,6 +12,7 @@ export {
   EarthquakeExplanation,
   EarthquakeCounts,
   EarthquakeHypocenterUpdate,
+  EarthquakeNankai,
   EewInformation,
   TsunamiInformation,
   WeatherTyphoon
---
+++ types/schema/earthquake-nankai/1.0.0.d.ts
@@ -0,0 +1,87 @@
+import { TelegramJSONMain } from '../../main';
+
+export namespace EarthquakeNankai {
+  export interface Schema {
+    type: 'earthquake-nankai';
+    version: '1.0.0';
+  }
+
+  export interface EarthquakeInfoKind {
+    code: string;
+    name: string;
+  }
+
+  export interface EarthquakeInfo {
+    kind?: EarthquakeInfoKind;
+    text: string;
+    appendix?: string;
+  }
+
+
+  export interface PublicBody {
+    earthquakeInfo?: EarthquakeInfo;
+    nextAdvisory?: string;
+    text?: string;
+  }
+
+  export interface CancelBody {
+    text: string;
+  }
+
+  export interface PublicVYSE50 extends TelegramJSONMain {
+    _schema: Schema;
+    type: '南海トラフ地震臨時情報';
+    title: string;
+    infoType: '発表' | '訂正';
+    targetDateTimeDubious: never;
+    targetDuration: never;
+    validDateTime: never;
+    eventId: string;
+    serialNo: null;
+    infoKind: '南海トラフ地震に関連する情報';
+    body: PublicBody;
+  }
+
+  export interface PublicVYSE51 extends TelegramJSONMain {
+    _schema: Schema;
+    type: '南海トラフ地震関連解説情報';
+    title: string;
+    infoType: '発表' | '訂正';
+    targetDateTimeDubious: never;
+    targetDuration: never;
+    validDateTime: never;
+    eventId: string;
+    serialNo: string;
+    infoKind: '南海トラフ地震に関連する情報';
+    body: PublicBody;
+  }
+
+  export interface PublicVYSE52 extends TelegramJSONMain {
+    _schema: Schema;
+    type: '南海トラフ地震関連解説情報';
+    title: string;
+    infoType: '発表' | '訂正';
+    targetDateTimeDubious: never;
+    targetDuration: never;
+    validDateTime: never;
+    eventId: string;
+    serialNo: null;
+    infoKind: '南海トラフ地震に関連する情報';
+    body: PublicBody;
+  }
+
+  export interface Cancel extends TelegramJSONMain {
+    _schema: Schema;
+    type: '南海トラフ地震臨時情報' | '南海トラフ地震関連解説情報';
+    title: string;
+    infoType: '取消';
+    targetDateTimeDubious: never;
+    targetDuration: never;
+    validDateTime: never;
+    eventId: string;
+    infoKind: '南海トラフ地震に関連する情報';
+    body: CancelBody;
+  }
+
+  export type Main = PublicVYSE50 | PublicVYSE51 | PublicVYSE52 | Cancel;
+}
