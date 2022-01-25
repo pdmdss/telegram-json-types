0eb091a800bcba8de261ca0435bc753ab2613ef8
soshi1822 <soshi1822@gmail.com>
2022-01-25 10:29:37 +0900

    add: JSON Schema 定義の作成スクリプト作成

---
+++ schema.ts
@@ -0,0 +1,53 @@
+import { resolve } from 'path';
+import { mkdir, readdir, rm, writeFile } from 'fs/promises';
+
+import * as TJS from 'typescript-json-schema';
+
+const basePath = resolve(__dirname, './types/schema/');
+const distDir = resolve(__dirname, './dist/schema/');
+
+
+const settings: TJS.PartialArgs = {
+  required: true,
+};
+
+const compilerOptions: TJS.CompilerOptions = {
+  strictNullChecks: true,
+};
+
+readdir(basePath)
+  .then(files => programTypescript(files));
+
+
+async function programTypescript(files: string[]) {
+  const tsTypeFiles = files.filter(file => /\.ts$/.test(file));
+  const filepath = tsTypeFiles.map(file => resolve(basePath, file));
+
+  const program = TJS.getProgramFromFiles(filepath, compilerOptions);
+  const generator = TJS.buildGenerator(program, settings);
+
+  const objectMaps = tsTypeFiles.map(file => tsTypeFileName2TypeSymbolName(file));
+
+  await rm(distDir, { recursive: true });
+  await mkdir(distDir, { recursive: true });
+  for (let i = 0; i < objectMaps.length; i++) {
+    const { schemaName, typeName } = objectMaps[i];
+
+    const schema = generator?.getSchemaForSymbol(typeName, true);
+
+    await writeFile(
+      resolve(distDir, `${schemaName}.json`),
+      JSON.stringify(schema, undefined, 4)
+    );
+  }
+}
+
+function tsTypeFileName2TypeSymbolName(file: string) {
+  const schemaName = file.replace(/^([\w-]+?)(\.d)?\.ts$/, '$1');
+  const nameScope = schemaName.replace(/-(\w)|(^\w)/g, (match, p1 = '', p2 = '') => `${p1}${p2}`.toUpperCase());
+
+  return {
+    schemaName,
+    typeName: nameScope + '.Main'
+  };
+}
