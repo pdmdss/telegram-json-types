3b0b57f58f81ad77b73d430152af40f5beb60116
soshi1822 <soshi1822@gmail.com>
2022-01-25 14:03:35 +0900

    add: earthquake-information の v1.0.0 をロールバックして用意

--- types/schema/earthquake-information/index.d.ts
+++ types/schema/earthquake-information/index.d.ts
@@ -1,9 +1,11 @@
 import { EarthquakeInformation as v1_1_0 } from './1.1.0';
+import { EarthquakeInformation as v1_0_0 } from './1.0.0';
 
 
 export {
   v1_1_0 as Latest,
-  v1_1_0
+  v1_1_0,
+  v1_0_0,
 };
 
 export default v1_1_0;
