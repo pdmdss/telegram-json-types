3de0badafda531ee10ca6dbc9d1a7298aae1aa25
soshi1822 <soshi1822@gmail.com>
2022-01-27 14:35:56 +0900

    add: スキーマチェックのテストを追加

---
+++ test/sample-schema-check.test.ts
@@ -0,0 +1,47 @@
+import Ajv from 'ajv';
+import fetch from 'node-fetch';
+import { getJSchema } from '../jschema/jschema-load';
+
+const checkSchemaTypes = [
+  'eew-information',
+  'earthquake-information',
+  'earthquake-explanation',
+  'earthquake-counts',
+  'earthquake-hypocenter-update',
+  'earthquake-nankai',
+  'tsunami-information',
+  'volcano-information',
+  'weather-typhoon',
+];
+
+const ajv = new Ajv({
+  strictTuples: false
+});
+
+test.each(checkSchemaTypes)('CheckSchemaType: %s', async schemaType => {
+  const sampleList = await getList(schemaType);
+
+  for (let i = 0; i < sampleList.length; i++) {
+    const url = sampleList[i];
+    const json: any = await fetch(url).then(res => res.json());
+    const schema = await getJSchema(json._schema.type, json._schema.version);
+    const validate = ajv.compile(schema);
+    const valid = validate(json);
+
+    console.group(url);
+    expect(validate.errors ?? []).toEqual([]);
+    console.groupEnd();
+  }
+});
+
+
+async function getList(schemaType: string) {
+  const url = `https://sample.dmdata.jp/conversion/json/schema/${schemaType}/`;
+  return await fetch(url).then(res => res.text())
+    .then(html => html
+      .replace(/^.+?<pre>.+?<hr>(.+?)<\/pre>.+?$/s, '$1')
+      .match(/<a\shref="(.+?)">/g)
+      ?.map(a => url + a.replace(/^<a\shref="(.+?)">$/, '$1'))
+      .filter(href => /\.json$/.test(href)) ?? []
+    );
+}
