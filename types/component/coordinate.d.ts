7d9d4bd484fd120c8785a1b99566721c7fe8cfe0
soshi1822 <soshi1822@gmail.com>
2022-06-26 16:49:15 +0900

    update: Coordinate component の定義を見やすくする
    Signed-off-by: soshi1822 <soshi1822@gmail.com>

--- types/component/coordinate.d.ts
+++ types/component/coordinate.d.ts
@@ -1,15 +1,27 @@
 import { UnitValue } from './unit-value';
 
-export type Coordinate<Geo = never> = {
-  latitude: { text: string, value: string };
-  longitude: { text: string, value: string };
+
+interface CoordinateNormal<Geo> {
+  latitude: {
+    text: string;
+    value: string;
+  };
+  longitude: {
+    text: string;
+    value: string;
+  };
   height?: UnitValue<'高さ', 'm'>;
   condition: never;
   geodeticSystem: Geo;
-} | {
+}
+
+interface CoordinateUnknown {
   latitude: never;
   longitude: never;
   height: never;
   geodeticSystem: never;
   condition: '不明';
 }
+
+
+export type Coordinate<Geo = never> = CoordinateNormal<Geo> | CoordinateUnknown;
