0527c99cd35d0ccb9018b0ff2b384c65facb1dcb
soshi1822 <soshi1822@gmail.com>
2022-09-11 12:58:16 +0900

    change: [eew-information] 定義を Lpgm から Lg に変更する
    Signed-off-by: soshi1822 <soshi1822@gmail.com>

--- types/schema/eew-information/1.0.0.d.ts
+++ types/schema/eew-information/1.0.0.d.ts
@@ -9,7 +9,7 @@ export namespace EewInformation {
   }
 
   export type IntensityClass = '0' | '1' | '2' | '3' | '4' | '5-' | '5+' | '6-' | '6+' | '7';
-  export type LpgmIntensityClass = '0' | '1' | '2' | '3' | '4';
+  export type LgIntensityClass = '0' | '1' | '2' | '3' | '4';
 
   export interface WarningAreaKindLastKind extends Components.CodeName {
   }
@@ -63,14 +63,14 @@ export namespace EewInformation {
     to: IntensityClass | 'over' | '不明';
   }
 
-  export interface IntensityForecastLpgmMaxInt {
-    from: LpgmIntensityClass | '不明';
-    to: LpgmIntensityClass | 'over' | '不明';
+  export interface IntensityForecastLgMaxInt {
+    from: LgIntensityClass | '不明';
+    to: LgIntensityClass | 'over' | '不明';
   }
 
   export interface IntensityAppendix {
     maxIntChange: '0' | '1' | '2';
-    maxLpgmIntChange?: '0' | '1' | '2';
+    maxLgIntChange?: '0' | '1' | '2';
     maxIntChangeReason: '0' | '1' | '2' | '3' | '4' | '9';
   }
 
@@ -79,7 +79,7 @@ export namespace EewInformation {
 
   export interface IntensityRegion extends Components.CodeName {
     forecastMaxInt: IntensityForecastMaxInt;
-    forecastLpgmMaxInt?: IntensityForecastLpgmMaxInt;
+    forecastLgMaxInt?: IntensityForecastLgMaxInt;
     isPlum: boolean;
     isWarning: boolean;
     kind: IntensityRegionKind;
@@ -108,7 +108,7 @@ export namespace EewInformation {
 
   export interface Intensity {
     forecastMaxInt: IntensityForecastMaxInt;
-    forecastLpgmMaxInt?: IntensityForecastLpgmMaxInt;
+    forecastLgMaxInt?: IntensityForecastLgMaxInt;
     appendix?: IntensityAppendix;
     regions: IntensityRegionItem[];
   }
