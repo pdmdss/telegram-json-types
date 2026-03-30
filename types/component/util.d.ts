e95c515315ae65b2493f8acddfb4b9bd2efbf923
soshi1822 <soshi1822@gmail.com>
2026-03-30 19:53:41 +0900

    fix: [util] unit が uint になっていた
    Signed-off-by: soshi1822 <soshi1822@gmail.com>

--- types/component/util.d.ts
+++ types/component/util.d.ts
@@ -1,7 +1,7 @@
 export namespace Util {
   export type Prohibit<T extends object, NK extends keyof T> = { [P in Exclude<keyof T, NK>]: T[P] } & { [P in Extract<keyof T, NK>]?: never; };
   export type ValueElement<T extends string, U extends string, Cv extends string | undefined = never, Cn extends string | undefined = '値なし'> =
-    { type: T; value: string; uint: U; condition?: Cv; } | { type: T; value: null; uint: U; condition: Cn; };
+    { type: T; value: string; unit: U; condition?: Cv; } | { type: T; value: null; unit: U; condition: Cn; };
 
   export type TimeRefID<T extends object> = { refId: string } & T;
   export type Local<T extends object> = { name: string; } & T;
